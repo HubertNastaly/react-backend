f6567bedac4d181e3217ddbf3c4a91dd69b8d5a3
HubertNastaly <h.nastaly@gmail.com>
2019-10-22 09:10:41 +0200

    InitialCommit

---
+++ src/main/java/pw/react/backend/reactbackend/ReactBackendLab3Application.java
@@ -0,0 +1,13 @@
+package pw.react.backend.reactbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBackendLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBackendLab3Application.class, args);
+	}
+
+}
